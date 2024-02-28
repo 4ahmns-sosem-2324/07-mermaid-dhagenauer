c59d81ffe033027981b187891c1efb61368e9510
DaveHagenauer <david.hagenauer05@gmail.com>
2024-02-28 10:00:45 +0100

    Scripts erstellen

---
+++ Assets/MyGame/Scripts/GameController.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameController : MonoBehaviour
+{
+    private GameObject player;
+    private GameObject enemy;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    
+}
